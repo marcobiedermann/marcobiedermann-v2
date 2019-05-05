83ba3fef4ec39b623a3f36cb5e355b34d2194fd4
Marco Biedermann <marco.biedermann@gmx.de>
2019-05-05 22:08:05 +0200

    refactor: layout
    abstract Layout into its own component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,8 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./Header"
-import "./layout.css"
+import Header from "../Header"
+import "./style.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
