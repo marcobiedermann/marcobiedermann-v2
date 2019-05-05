196f999281c9a60e68efbbad0b9293f9ec02e7c2
Marco Biedermann <marco.biedermann@gmx.de>
2019-05-05 22:10:33 +0200

    style: imports
    organize imports

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,6 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
-import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
+import React from "react"
 import Header from "../Header"
 import "./style.css"
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import React from "react"
 import { Link } from "gatsby"
-
+import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
