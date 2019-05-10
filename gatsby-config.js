bda3926243975d80f98393253cd54fcb4e28ece8
Marco Biedermann <marco.biedermann@gmx.de>
2019-05-10 11:40:31 +0200

    docs: page into
    change page info in header meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Marco Biedermann`,
+    description: `Full-Stack JavaScript Engineer with focus on React, Redux & Node.js and passioned about Open Source working @moovel based in Berlin, Germany`,
+    author: `@m412c0b`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
