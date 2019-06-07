c2ad83b4b5522999744f67cbe30f05ca7116dbc0
Marco Biedermannn <marco.biedermann@gmx.de>
2019-06-07 09:12:31 +0200

    feat: project images
    render project images

--- src/templates/Project/index.jsx
+++ src/templates/Project/index.jsx
@@ -1,4 +1,5 @@
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Layout from '../../components/Layout';
@@ -7,26 +8,69 @@ import Section from '../../components/Section';
 const ProjectTemplate = props => {
   const {
     data: {
-      contentfulProject: { agency, client, role, title },
+      contentfulProject: { agency, client, colors, desktop, homepage, mobile, tablet, role, title },
     },
   } = props;
 
   return (
     <Layout>
+      {desktop && (
+        <Section>
+          <Img fluid={desktop.fluid} />
+        </Section>
+      )}
+
       <Section>
         <h1>{title}</h1>
         <ul>
-          <li>
-            <strong>Client:</strong> {client}
-          </li>
-          <li>
-            <strong>Agency:</strong> {agency}
-          </li>
-          <li>
-            <strong>Role:</strong> {role}
-          </li>
+          {client && (
+            <li>
+              <strong>Client:</strong> {client}
+            </li>
+          )}
+
+          {agency && (
+            <li>
+              <strong>Agency:</strong> {agency}
+            </li>
+          )}
+
+          {role && (
+            <li>
+              <strong>Role:</strong> {role}
+            </li>
+          )}
         </ul>
       </Section>
+
+      {colors && (
+        <Section>
+          <h2>Colors</h2>
+          <Img fluid={colors.fluid} />
+        </Section>
+      )}
+
+      {homepage && (
+        <Section>
+          <h2>Homepage</h2>
+          <Img fluid={homepage.fluid} />
+        </Section>
+      )}
+
+      {tablet && (
+        <Section>
+          <h2>Tablet view</h2>
+          <Img fluid={tablet.fluid} />
+        </Section>
+      )}
+
+      {mobile && (
+        <Section>
+          <h2>Mobile View</h2>
+          <p>With a small display comes great responsibilities</p>
+          <Img fluid={mobile.fluid} />
+        </Section>
+      )}
     </Layout>
   );
 };
@@ -51,9 +95,34 @@ export const pageQuery = graphql`
     contentfulProject(slug: { eq: $slug }) {
       agency
       client
+      colors {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+      }
+      desktop {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+      }
+      homepage {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+      }
       id
+      mobile {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+      }
       role
       slug
+      tablet {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+      }
       tags
       title
       url
