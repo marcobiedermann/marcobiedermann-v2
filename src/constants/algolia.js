68b3df319e3937cd8d5aaf21182e3791c373aae1
Marco Biedermannn <marco.biedermann@gmx.de>
2019-06-07 12:20:04 +0200

    fix: env
    fix environment variables in browser

--- src/constants/algolia.js
+++ src/constants/algolia.js
@@ -1,4 +1,4 @@
 /* eslint-disable prefer-destructuring */
 
-export const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+export const ALGOLIA_API_KEY = process.env.GATSBY_ALGOLIA_API_KEY;
 export const ALGOLIA_APP_ID = 'MJNYMFM42R';
