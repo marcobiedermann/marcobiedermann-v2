fd8b74c6f2e1fc846113c2ddc31f4fb7b805de5e
Marco Biedermannn <marco.biedermann@gmx.de>
2019-06-15 18:19:58 +0200

    feat: figure styles
    add figure styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import 'typeface-montserrat';
 import './src/styles/anchor.css';
 import './src/styles/body.css';
 import './src/styles/button.css';
+import './src/styles/figure.css';
 import './src/styles/headline.css';
 import './src/styles/image.css';
 import './src/styles/input.css';
