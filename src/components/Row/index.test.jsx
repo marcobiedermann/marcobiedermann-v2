3ee387c5a478a76be2bcfe37678e7df64bed079a
Marco Biedermannn <marco.biedermann@gmx.de>
2019-06-27 09:23:00 +0200

    feat: row
    add row component

---
+++ src/components/Row/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Row from '.';
+
+describe('Row component', () => {
+  it('should render correctly', () => {
+    const tree = renderer.create(<Row />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
