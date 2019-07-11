270895d5c1215857a518fe1db0de802381ffbcba
Marco Biedermann <marco.biedermann@gmx.de>
2019-07-11 08:48:25 +0200

    feat: page-2
    remove page-2

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -8,11 +8,6 @@ export const INDEX = {
   path: '/',
 };
 
-export const PAGE_2 = {
-  name: 'Page 2',
-  path: '/page-2',
-};
-
 export const PROJECTS = {
   name: 'Projects',
   path: '/projects',
