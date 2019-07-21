6b2c524789664c3de1b6fcac0c7dbd8854776620
Marco Biedermann <marco.biedermann@gmx.de>
2019-07-21 13:24:09 +0200

    style: line-length
    change line-length to 120 characters

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -5,9 +5,7 @@ import React from 'react';
 const Logo = props => {
   const { logo, slug } = props;
 
-  return (
-    <Link to={`/projects/${slug}`}>{logo && <img src={logo.file.url} alt={logo.title} />}</Link>
-  );
+  return <Link to={`/projects/${slug}`}>{logo && <img src={logo.file.url} alt={logo.title} />}</Link>;
 };
 
 Logo.propTypes = {
