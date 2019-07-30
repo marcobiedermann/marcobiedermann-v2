9d2d5c15df829b044ce5304aee4422c7a42df7dd
Marco Biedermann <marco.biedermann@gmx.de>
2019-07-30 08:43:19 +0200

    chore: type checking
    Add @types and add type checking

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,3 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-
+/**
+ *
+ * @param {string} string
+ * @returns {string}
+ */
 export const capitalize = string => `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
