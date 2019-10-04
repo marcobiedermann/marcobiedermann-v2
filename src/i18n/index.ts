414b41e40823b582ec41b72c271b2c5a7e071e30
Marco Biedermann <marco.biedermann@gmx.de>
2019-10-05 00:27:24 +0200

    fix: locales
    Fix import of locales

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,20 +1,24 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
+import about from '../locales/en/about.json';
+import common from '../locales/en/common.json';
+import project from '../locales/en/project.json';
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    defaultNS: 'common',
-    debug: true,
-    fallbackLng: 'en',
-    ns: ['about', 'common', 'project'],
-    interpolation: {
-      escapeValue: false,
+i18n.use(initReactI18next).init({
+  defaultNS: 'common',
+  debug: true,
+  fallbackLng: 'en',
+  ns: ['about', 'common', 'project'],
+  resources: {
+    en: {
+      about,
+      common,
+      project,
     },
-  });
+  },
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
