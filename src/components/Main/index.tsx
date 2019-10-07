af6606aeac40cf92f42b48a14bbe15514ac42df0
Marco Biedermann <marco.biedermann@gmx.de>
2019-10-07 21:55:36 +0200

    fix: main
    Remove unused empty class from main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,18 +1,9 @@
-import classNames from 'classnames';
 import React from 'react';
 
-export interface MainProps {
-  className?: string;
-}
+const Main: React.FC = props => {
+  const { children, ...otherProps } = props;
 
-const Main: React.FC<MainProps> = props => {
-  const { children, className, ...otherProps } = props;
-
-  return (
-    <main className={classNames(className)} {...otherProps}>
-      {children}
-    </main>
-  );
+  return <main {...otherProps}>{children}</main>;
 };
 
 export default Main;
