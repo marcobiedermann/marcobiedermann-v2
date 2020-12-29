0076607b02b164c5c76633d19668234e7269de22
Marco Biedermann <marco.biedermann@gmx.de>
2020-12-29 16:07:33 +0100

    test: Jest setup
    Update Jest setup

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const babelOptions = {
-  presets: ['babel-preset-gatsby'],
+  presets: ['babel-preset-gatsby', '@babel/preset-typescript'],
 };
 
 module.exports = require('babel-jest').createTransformer(babelOptions);
